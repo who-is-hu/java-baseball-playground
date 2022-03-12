02fc58e8de6f676dbfb42991a75d37804abac922
who-is-hu <dydgndlaek97@gmail.com>
2022-03-13 01:30:20 +0900

    feat: 입력 클래스

---
+++ src/main/java/InputManager.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class InputManager {
+    private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public ArrayList<Integer> inputAnswer() throws IOException {
+        System.out.println("input three numbers : ");
+        String input = reader.readLine();
+        StringTokenizer tokens = new StringTokenizer(input, " ");
+
+        ArrayList<Integer> answer = new ArrayList<>();
+        while(tokens.hasMoreTokens()){
+            answer.add(Integer.parseInt(tokens.nextToken()));
+        }
+        return answer;
+    }
+}
