d3e0ac00c537b7103f95312c7c262f26cf6e2ba7
who-is-hu <dydgndlaek97@gmail.com>
2022-03-13 20:54:35 +0900

    feat: 출력 클래스

--- src/main/java/OutputManager.java
+++ src/main/java/OutputManager.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class OutputManager {
+import java.util.HashMap;
+
+public class OutputManager {
+
+    public static void showInningResult(HashMap<Type, Integer> judgement) {
+        if(judgement.get(Type.MISS) == AnswerGenerator.ANSWER_DIGIT) {
+            System.out.println("miss");
+            return;
+        }
+        System.out.printf("%d strike %d ball\n", judgement.get(Type.STRIKE), judgement.get(Type.BALL));
+    }
 }
